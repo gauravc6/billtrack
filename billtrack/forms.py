e3537ee986b28372b36535ff909cb4b8b161801c
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2020-05-10 21:13:44 +0530

    create Product model

--- billtrack/forms.py
+++ billtrack/forms.py
@@ -7,5 +7,14 @@ class BillForm(FlaskForm):
     invoice_number = StringField('Invoice Number', validators=[DataRequired()])
     supplier_name = StringField('Supplier Name', validators=[DataRequired()])
     bill_total = FloatField('Bill total', validators=[DataRequired()])
-    bill_paid_status = BooleanField('Bill Paid', validators=[DataRequired()])
+    bill_paid_status = BooleanField('Bill Paid')
     submit = SubmitField('Add bill')
+
+class ProductForm(FlaskForm):
+    product_name = StringField('Product Name', validators=[DataRequired()])
+    product_pack = StringField('Product pack', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    free_quantity = IntegerField('Free quantity')
+    mrp = FloatField('MRP', validators=[DataRequired()])
+    rate = FloatField('Rate', validators=[DataRequired()])
+    submit = SubmitField('Add product')
--- billtrack/models.py
+++ billtrack/models.py
@@ -10,4 +10,20 @@ class Bill(db.Model):
     supplier_name = db.Column(db.Text, nullable=False)
     bill_total = db.Column(db.Float, nullable=False)
     bill_paid_status = db.Column(db.Boolean, default=False)
+
+    products = db.relationship('Product', backref='bill', lazy=True)
+
+class Product(db.Model):
+
+    __tablename__ = 'product'
+
+    id = db.Column(db.Integer, primary_key=True)
+    bill_id = db.Column(db.Integer, db.ForeignKey('bill.id'), nullable=False)
+
+    product_name = db.Column(db.Text, nullable=False)
+    product_pack = db.Column(db.String(10), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    free_quantity = db.Column(db.Integer)
+    mrp = db.Column(db.Float, nullable=False)
+    rate = db.Column(db.Float, nullable=False)
    
