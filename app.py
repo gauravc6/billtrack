6abe92485816a947304f1bf22d943895d2eea36b
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2020-05-10 21:30:25 +0530

    add routes for Product

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from flask import render_template, redirect, url_for
 from billtrack import app, db
-from billtrack.models import Bill
-from billtrack.forms import BillForm
+from billtrack.models import Bill, Product
+from billtrack.forms import BillForm, ProductForm
 
 @app.route('/')
 def index():
@@ -29,8 +29,25 @@ def addbill():
 @app.route('/<invoice_number>')
 def bill_details(invoice_number):
     bill = Bill.query.filter_by(invoice_number=invoice_number).first_or_404()
-    return render_template('bill_details.html', bill=bill)
+    products = Product.query.filter_by(bill_id=bill.id).all()
+    return render_template('bill_details.html', bill=bill, products=products)
 
+@app.route('/<invoice_number>/addproduct', methods=['GET', 'POST'])
+def addproduct(invoice_number):
+    form = ProductForm()
+    if form.validate_on_submit():
+        bill = Bill.query.filter_by(invoice_number=invoice_number).first()
+        product = Product(bill_id=bill.id,
+                        product_name=form.product_name.data,
+                        product_pack=form.product_pack.data,
+                        quantity=form.quantity.data,
+                        free_quantity=form.free_quantity.data,
+                        mrp=form.mrp.data,
+                        rate=form.rate.data)
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('bill_details', invoice_number=invoice_number))
+    return render_template('addproduct.html', form=form)
 
 
 if __name__ == "__main__":
